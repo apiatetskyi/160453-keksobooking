ec752d23d948294b742ac34720868a2ed3e81ff7
Aleksei Piatetskyi <a.piatetskyi@gmail.com>
2018-02-18 22:16:05 +0200

    Реализовал перетаскивание пина

--- js/map.js
+++ js/map.js
@@ -2,8 +2,12 @@
 
 (function () {
   var ADS_COUNT = 8;
+  var MIN_Y = 150;
+  var MAX_Y = 500;
+  var POINTER_HEIGHT = 19;
 
   var adsData;
+  var activated = false;
   var pins = [];
   var map = document.querySelector('.map');
   var mainPin = map.querySelector('.map__pin--main');
@@ -27,6 +31,8 @@
     pins = [];
     map.classList.add('map--faded');
     window.form.deactivate();
+    mainPin.style = false;
+    activated = false;
   };
 
   /**
@@ -63,16 +69,63 @@
 
   /**
    * Обработчик mouseup на главном пине
+   * @param {Object} evt
    */
-  var mainPinMouseupHandler = function () {
-    activate();
-    window.form.activate();
-    renderPins(adsData, pinsContainer);
-    window.form.setLocation(mainPin.offsetLeft, mainPin.offsetTop);
+  var mainPinMousedownHandler = function (evt) {
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var mousemoveHandler = function (moveEvt) {
+      var offset = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if (mainPin.offsetTop < MIN_Y) {
+        mainPin.style.top = MIN_Y + 'px';
+      } else if (mainPin.offsetTop > MAX_Y) {
+        mainPin.style.top = MAX_Y + 'px';
+      } else {
+        mainPin.style.top = mainPin.offsetTop - offset.y + 'px';
+      }
+
+      if (mainPin.offsetLeft < 0) {
+        mainPin.style.left = 0;
+      } else if (mainPin.offsetLeft > pinsContainer.offsetWidth) {
+        mainPin.style.left = pinsContainer.offsetWidth + 'px';
+      } else {
+        mainPin.style.left = mainPin.offsetLeft - offset.x + 'px';
+      }
+
+      window.form.setLocation(mainPin.offsetLeft, mainPin.offsetTop);
+    };
+
+    var mouseupHandler = function () {
+      if (!activated) {
+        activate();
+        window.form.activate();
+        renderPins(adsData, pinsContainer);
+        activated = true;
+        mainPin.style.transform = 'translate(-50%, calc(-100% - ' + POINTER_HEIGHT + 'px))';
+      }
+      window.form.setLocation(mainPin.offsetLeft, mainPin.offsetTop);
+      document.removeEventListener('mousemove', mousemoveHandler);
+      document.removeEventListener('mouseup', mouseupHandler);
+    };
+
+    document.addEventListener('mousemove', mousemoveHandler);
+    document.addEventListener('mouseup', mouseupHandler);
   };
 
   adsData = getDataArray(ADS_COUNT);
-  mainPin.addEventListener('mouseup', mainPinMouseupHandler);
+  mainPin.addEventListener('mousedown', mainPinMousedownHandler);
 
   window.map = {
     element: map,
